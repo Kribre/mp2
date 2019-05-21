36d092b5d0a6dfe85da4c0633f3cf79b86a4c7fb
Jenny Johannessen <jenn3108@edu.eal.dk>
2019-05-21 19:52:32 +0200

    classes og js
    Tilføjet to classes i css'en for at toppanelhelet fylder det hele og overskriften ender i midten.
    
    TIlføjet script.

--- introduktion.js
+++ introduktion.js
@@ -1,4 +1,11 @@
 function scrollAppear(){
-    var visualEffect = document.querySelector('visual-effect');
+    var visualEffect = document.querySelector('.visual-effect');
     var visualPosition = visualEffect.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 2;
+    
+    if(visualPosition < screenPosition){
+        visualEffect.classList.add('visual-effect-appear');
+    }
 }
+
+window.addEventListener('scroll', scrollAppear);
