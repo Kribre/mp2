db105974b2975f1455ffd0b476cc19f2bbd7adf3
Jenny Johannessen <jenn3108@edu.eal.dk>
2019-05-21 20:20:29 +0200

    laver billede helvisning og overskrift i midten

--- introduktion.js
+++ introduktion.js
@@ -1,7 +1,7 @@
 function scrollAppear(){
     var visualEffect = document.querySelector('.visual-effect');
     var visualPosition = visualEffect.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight / 2;
+    var screenPosition = window.innerHeight / 1.2;
     
     if(visualPosition < screenPosition){
         visualEffect.classList.add('visual-effect-appear');
