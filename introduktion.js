3894cb8b53f49e64502332ca660b51a79a685121
Jenny Johannessen <jenn3108@edu.eal.dk>
2019-05-22 11:07:04 +0200

    javascript og css/html til javascript

--- introduktion.js
+++ introduktion.js
@@ -1,11 +1,42 @@
-function scrollAppear(){
+//javascript til text fade-in
+
+function scrollAppear() {
     var visualEffect = document.querySelector('.visual-effect');
     var visualPosition = visualEffect.getBoundingClientRect().top;
     var screenPosition = window.innerHeight / 1.2;
-    
-    if(visualPosition < screenPosition){
+
+    if (visualPosition < screenPosition) {
         visualEffect.classList.add('visual-effect-appear');
     }
 }
 
 window.addEventListener('scroll', scrollAppear);
+
+
+
+
+//javascript til accordion åbner
+
+document.addEventListener('click', function (event) {
+    if (!event.target.classList.contains('accordion-toggle')) return;
+
+
+    var content = document.querySelector(event.target.hash);
+    if (!content) return;
+
+
+    event.preventDefault();
+
+    if (content.classList.contains('active')) {
+        content.classList.remove('active');
+        return;
+    }
+
+    var accordions = document.querySelectorAll('.accordion');
+
+    for (var i = 0; i < accordions.length; i++) {
+        accordions[i].classList.remove('active');
+    }
+    content.classList.add('active');
+
+}, false);
